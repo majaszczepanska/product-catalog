c4b4a4214dd51ccf4188a3aed227d42218137265
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-03-12 13:46:57 +0100

    create table for producers

---
+++ src/main/java/pl/majaszczepanska/product_catalog/model/Producer.java
@@ -0,0 +1,34 @@
+package pl.majaszczepanska.product_catalog.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "producers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "producer", cascade = CascadeType.ALL)
+    private List<Product> products;
+}
