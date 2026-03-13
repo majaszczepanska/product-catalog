32c1e312ca1f53379b30ef060a1712431553b78b
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-03-13 12:04:40 +0100

    add validation

--- src/main/java/pl/majaszczepanska/product_catalog/dto/ProductRequest.java
+++ src/main/java/pl/majaszczepanska/product_catalog/dto/ProductRequest.java
@@ -3,13 +3,24 @@ package pl.majaszczepanska.product_catalog.dto;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class ProductRequest {
+    @NotBlank(message = "Name is required")
     private String name;
+
     private String description;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive number")
     private BigDecimal price;
+
+    @NotNull(message = "Producer ID is required")
     private Long producerId;
+    
     private Map<String, Object> attributes;
 }
